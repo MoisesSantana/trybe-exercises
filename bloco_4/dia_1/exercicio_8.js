117066fd7884922647f94bbb225b5c917eda0158
MoisesSantana <moisaant@gmail.com>
2020-08-27 18:09:04 -0300

    exercicio_8/OK

---
+++ bloco_4/dia_1/exercicio_8.js
@@ -0,0 +1,12 @@
+const positivoOuNegativo = 1!=1;
+
+switch(positivoOuNegativo){
+    case true:
+        console.log('positive');
+        break;
+    case false:
+        console.log('negative');
+        break;
+    default:
+        console.log('zero')
+}
