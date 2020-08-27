10a98d35056452cbf951e4cee0bff88352ccec72
MoisesSantana <moisaant@gmail.com>
2020-08-27 18:15:39 -0300

    exercicio_9/OK

---
+++ bloco_4/dia_1/exercicio_9.js
@@ -0,0 +1,5 @@
+const primeiraAngulacao = 150;
+const segundaAngulacao = 20;
+const terceiraAngulacao = 10;
+
+( primeiraAngulacao + segundaAngulacao + terceiraAngulacao ) == 180 ? console.log(`A soma dos 3 angulos ${primeiraAngulacao}, ${segundaAngulacao} e ${terceiraAngulacao} forma um triangulo`) : console.log(`A soma dos 3 angulos ${primeiraAngulacao}, ${segundaAngulacao} e ${terceiraAngulacao} não forma um triangulo`)
