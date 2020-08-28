5c84fd0b603212a8d2c588d7cbf7030d27080e0d
MoisesSantana <moisaant@gmail.com>
2020-08-28 11:28:36 -0300

    exercicio_14/Estrutura condicional OK

---
+++ bloco_4/dia_1/exercicio_14.js
@@ -0,0 +1,27 @@
+const salarioBruto = 3000
+let salarioLiquido = 0
+
+if ( salarioBruto <= 0 ) {
+    console.log('Valor incorreto')
+} else {
+    if ( salarioBruto <= 1556.94 ) {
+
+    } else if ( salarioBruto <= 2594.92 ) {
+
+    } else if ( salarioBruto <= 5189.82 ) {
+
+    } else {
+
+    }
+    if ( salarioLiquido > 4664.68 ) {
+
+    } else if ( salarioLiquido >= 3751.06 ) {
+
+    } else if ( salarioLiquido >= 2826.66 ) {
+
+    } else if ( salarioLiquido >= 1903.99 ) {
+
+    } else {
+        
+    }
+}
