70d6835d643b797541d30d209b9be03a084b623e
MoisesSantana <moisaant@gmail.com>
2020-09-01 08:56:52 -0300

    exercicio_4/OK

--- bloco_4/dia_3/exercicio_4.js
+++ bloco_4/dia_3/exercicio_4.js
@@ -1,12 +1,17 @@
 const n = 5;
 let linha = '';
+let nLinhas = Math.round(n / 2);
+let contEspacos = Math.floor(n / 2);
 
-for (let i = 1; i <= n; i += 1) {
+for (let i = 1; i <= nLinhas; i += 1) {
   linha = '';
-  if (i % 2 != 0){
-    for (let j = i; j >= 1; j -= 1) {
+  for (j = 1; j <= n; j += 1) {
+    if (j <= contEspacos || j > n - contEspacos) {
+      linha += ' ';
+    } else {
       linha += '*';
     }
-    console.log(linha);
   }
+  console.log(linha);
+  contEspacos -= 1;
 }
