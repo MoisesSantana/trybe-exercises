0a1512e9ccd159ca048053fc32959e54325ec074
MoisesSantana <moisaant@gmail.com>
2020-09-17 19:03:59 -0300

    first fieldset

---
+++ bloco_6/dia_1/exercicio_1/script.js
@@ -0,0 +1,8 @@
+const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+const stateComboBox = document.querySelector('#state');
+
+for (let index = 0; index < states.length; index += 1) {
+  const newStateOption = document.createElement('option');
+  newStateOption.innerText = states[index];
+  stateComboBox.appendChild(newStateOption);
+}
