c048fcddc43a050b2c68fdfb06da8b875b4c6ef7
MoisesSantana <moisaant@gmail.com>
2020-09-28 19:18:20 -0300

    exercicios 1 ao 2/6

---
+++ bloco_7/dia_2/exercicio_3.js
@@ -0,0 +1,52 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const tarefa1 = (lesson2, turno, horario) => lesson2[turno] = horario;
+
+console.log(tarefa1(lesson2, 'turno', 'manhã'));
+console.log(lesson2);
+
+const tarefa2 = (lesson1) => Object.keys(lesson1);
+
+console.log(tarefa2(lesson1));
+
+const tarefa3 = (lesson2) => Object.keys(lesson2).length;
+
+console.log(tarefa3(lesson2));
+
+const tarefa4 = (lesson3) => Object.values(lesson3);
+
+console.log(tarefa4(lesson3));
+
+//tarefa 5
+let allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+console.log(allLessons);
+
+const tarefa6 = (allLessons) => {
+  let result = allLessons.lesson1['numeroEstudantes'];
+  console.log(result)
+  result += allLessons.lesson2['numeroEstudantes'];
+  console.log(result)
+  result += allLessons.lesson3['numeroEstudantes'];
+  return result;
+}
+
+console.log(tarefa6(allLessons));
