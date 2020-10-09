e733770f6f23625340978e04a4262c2749d04a6e
MoisesSantana <moisaant@gmail.com>
2020-10-09 13:59:16 -0300

    exercicio_5 - exercicio_7 - OK

--- bloco_8/dia_4/exercicio_5.js
+++ bloco_8/dia_4/exercicio_5.js
@@ -1,11 +1,15 @@
 const assert = require('assert')
 
-const palio = ["Palio", "Fiat", 2019]
-const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
-const chiron = ["Chiron", "Bugatti", 2016]
+const myList = [1, 2, 3];
 
-// escreva toObject abaixo
+// escreva swap abaixo
 
-assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
-assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
-assert.deepEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
+// Escreva a função swap, que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+
+const swap = ([first, second, third]) => [third, second, first];
+
+const swappedList = swap(myList);
+
+assert.equal(swappedList[0], 3);
+assert.equal(swappedList[1], 2);
+assert.equal(swappedList[2], 1);
